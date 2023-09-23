773db006a02576c87c44b4df47091564db94eb8c
dnlsboev <dnlsboev@gmail.com>
2023-09-23 23:11:40 +0700

    new file:   Asimov Profession ENG.txt 	new file:   Atlas Shrugged.txt 	new file:   main.cpp 	new file:   test1.txt

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <ctime>
+
+using namespace std; 
+
+int main()
+{
+    map<char, int> frequencies; 
+
+    // инициализация
+    for(int c {97}; c < 123; c++)
+        frequencies[(char) c] = 0;
+
+    clock_t time = clock();
+    
+    string file_name = "Asimov Profession ENG.txt";
+
+    ifstream input_file(file_name);
+    if(input_file.is_open())
+    {
+        string line;
+
+        while(getline(input_file, line))
+            for (char c : line)
+                if (isalpha(c))
+                    frequencies[(char) tolower(c)]++;
+    }
+    input_file.close();
+
+    float full_time = (float) (clock() - time) / CLOCKS_PER_SEC;
+
+
+    cout << "File name: " << file_name << endl;
+    cout << "Time: " << full_time << " sec." << endl;
+
+    // вывод частот
+    for (const auto& [character, freq] : frequencies)
+        cout << character << ": " << freq << endl;
+}
+
+// a -- 97, z -- 122 --- англ алфавит
