b182b17a633541b4589c0b517fe627aa68741828
dnlsboev <dnlsboev@gmail.com>
2023-11-27 23:49:55 +0700

    OpenMP solution is ready.

--- main.cpp
+++ main.cpp
@@ -3,40 +3,94 @@
 #include <map>
 #include <string>
 #include <ctime>
+#include <vector>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 
 using namespace std; 
 
+
 int main()
 {
-    map<char, int> frequencies; 
+    #ifdef _OPENMP
+    cout << "Caution: This program was compiled with " 
+            "OpenMP and can consume all CPU resourses "
+            "of your PC!" << endl;
+    #endif
+     
+    vector<string> lines;
+   
 
-    // init
-    for(int c {97}; c < 123; c++)
-        frequencies[(char) c] = 0;
-
-    clock_t time = clock();
-    
-    string file_name = "Asimov Profession ENG.txt";
-
-    ifstream input_file(file_name);
-    if(input_file.is_open())
-    {
+    // openning file and getting lines in the vector
+    string filename = "Bible50.txt";
+    ifstream input_file;
+    input_file.open(filename);
+    if(input_file.is_open()){
         string line;
-
-        while(getline(input_file, line))
-            for (char c : line)
-                if (isalpha(c))
-                    frequencies[(char) tolower(c)]++;
+        while (getline(input_file, line))
+            lines.push_back(line);
     }
+
     input_file.close();
 
-    float full_time = (float) (clock() - time) / CLOCKS_PER_SEC;
+
+    int num_of_lines = lines.size();
+
+    int num_threads;
+    cout << "Enter num of threads" << endl;
+    cin >> num_threads;
+    omp_set_num_threads(num_threads);
+
+    cout << "THIS PROGRAM USE " << num_threads << " THREADS" << endl;
+
+    // initialization of frequencies
+    unsigned long int frequencies[26];
+    
+    for(int i = 0; i < 26; i++)
+        frequencies[i] = 0;
 
 
-    cout << "File name: " << file_name << endl;
-    cout << "Time: " << full_time << " sec." << endl;
+    double exec_time;
+    #ifdef _OPENMP
+        double itime = omp_get_wtime();
+    #else 
+        clock_t itime = clock();
+    #endif
+ 
 
-    // out
-    for (const auto& [character, freq] : frequencies)
-        cout << character << ": " << freq << endl;
+    #pragma omp parallel for reduction(+: frequencies)
+    for (int i = 0; i < num_of_lines; i++){     
+        for (int j =0; j < size(lines[i]); j++){
+            if (isalpha(lines[i][j]))
+                frequencies[(int) tolower(lines[i][j]) - 97] += 1;
+        }
+    }
+        
+
+    #ifdef _OPENMP
+        double ftime = omp_get_wtime();
+        exec_time = ftime - itime;
+    #else 
+        clock_t ftime = clock();
+        exec_time = (double) (ftime - itime) / CLOCKS_PER_SEC;
+    #endif  
+
+
+    cout << "File name: " << filename << endl;
+    cout << "Num of lines = " << num_of_lines << endl;
+    cout << "Time: " << exec_time << " sec." << endl;
+
+
+    string answer;
+    cout << "Show freq of symbols (y/n)? " << endl;
+    cin >> answer;
+    if (answer == "y" or answer == "Y"){                
+        for (int i = 0; i < 26; i ++)
+            cout << (char)(i+97) << ": " << frequencies[i] << endl;
+    }
+
+    return 0;
 }
